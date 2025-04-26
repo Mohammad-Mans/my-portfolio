be8241f0b64a51ea7fbfb15d683864a2655b5193
Mohammad Mansour <mohammed.i02@hotmail.com>
2025-04-26 17:56:18 +0300

    Add Snackbar for Contact Section

--- src/pages/HomePage/components/Contact/index.tsx
+++ src/pages/HomePage/components/Contact/index.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { TextField, Button, Box, Alert, styled, CircularProgress } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Box,
+  Alert,
+  styled,
+  CircularProgress,
+  Snackbar,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import Lottie from "lottie-react";
 import MessageAnimation from "../../../../assets/lotties/Message.json";
@@ -23,9 +31,12 @@ const StyledTextField = styled(TextField)(({ theme }) => ({
 const ContactSection: React.FC = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [success, setSuccess] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+  const [snackbar, setSnackbar] = useState<{
+    open: boolean;
+    message: string;
+    severity: "success" | "error";
+  } | null>(null);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -41,106 +52,117 @@ const ContactSection: React.FC = () => {
       );
 
       if (response.status === 200) {
-        setSuccess("Email sent successfully!");
-        setError(null);
+        setSnackbar({
+          open: true,
+          message: "Email sent successfully!",
+          severity: "success",
+        });
         setEmail("");
         setMessage("");
       }
     } catch (err) {
-      setError("Failed to send email. Please try again.");
       console.log(err);
-      setSuccess(null);
+      setSnackbar({
+        open: true,
+        message: "Failed to send email. Please try again.",
+        severity: "error",
+      });
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <Box
-      id="contact"
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      py={4}
-    >
-      <SectionTitle title="Contact" />
+    <>
+      <Box
+        id="contact"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        py={4}
+      >
+        <SectionTitle title="Contact" />
 
-      <Box display="flex" justifyContent="center">
-        <Box
-          display="flex"
-          flexDirection="column"
-          flex={2}
-          justifyContent="center"
-        >
-          <Box component="form" onSubmit={handleSubmit}>
-            {success && (
-              <Alert severity="success" sx={{ mb: 2 }}>
-                {success}
-              </Alert>
-            )}
-            {error && (
-              <Alert severity="error" sx={{ mb: 2 }}>
-                {error}
-              </Alert>
-            )}
+        <Box display="flex" justifyContent="center">
+          <Box
+            display="flex"
+            flexDirection="column"
+            flex={2}
+            justifyContent="center"
+          >
+            <Box component="form" onSubmit={handleSubmit}>
+              <StyledTextField
+                fullWidth
+                label="Your Email"
+                type="email"
+                variant="outlined"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
 
-            <StyledTextField
-              fullWidth
-              label="Your Email"
-              type="email"
-              variant="outlined"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
+              <StyledTextField
+                fullWidth
+                label="Your Message"
+                multiline
+                minRows={6}
+                variant="outlined"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              />
+
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                endIcon={!loading && <SendIcon />}
+                disabled={loading}
+                sx={{
+                  border: (theme) => `1px solid ${theme.palette.primary.main}`,
+                  borderRadius: "30px",
+                  paddingY: (theme) => theme.spacing(1.5),
+                  minWidth: "150px",
+                }}
+              >
+                {loading ? (
+                  <CircularProgress size={24} sx={{ color: "white" }} />
+                ) : (
+                  "Send Message"
+                )}
+              </Button>
+            </Box>
+          </Box>
+          <Box
+            display={{ xxs: "none", md: "flex" }}
+            flex={1}
+            ml={4}
+            maxHeight={400}
+          >
+            <Lottie
+              animationData={MessageAnimation}
+              loop={true}
+              style={{ height: "auto", width: "100%" }}
             />
-
-            <StyledTextField
-              fullWidth
-              label="Your Message"
-              multiline
-              minRows={6}
-              variant="outlined"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              required
-            />
-
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              endIcon={!loading && <SendIcon />}
-              disabled={loading}
-              sx={{
-                border: (theme) => `1px solid ${theme.palette.primary.main}`,
-                borderRadius: "30px",
-                paddingY: (theme) => theme.spacing(1.5),
-                minWidth: "150px",
-              }}
-            >
-              {loading ? (
-                <CircularProgress size={24} sx={{ color: "white" }} />
-              ) : (
-                "Send Message"
-              )}
-            </Button>
           </Box>
         </Box>
-        <Box
-          display={{ xxs: "none", md: "flex" }}
-          flex={1}
-          ml={4}
-          maxHeight={400}
-        >
-          <Lottie
-            animationData={MessageAnimation}
-            loop={true}
-            style={{ height: "auto", width: "100%" }}
-          />
-        </Box>
       </Box>
-    </Box>
+      <Snackbar
+        open={snackbar?.open || false}
+        autoHideDuration={4000}
+        onClose={() => setSnackbar(null)}
+        anchorOrigin={{ vertical: "top", horizontal: "left" }}
+      >
+        <Alert
+          onClose={() => setSnackbar(null)}
+          severity={snackbar?.severity}
+          sx={{ width: "100%" }}
+        >
+          {snackbar?.message}
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
